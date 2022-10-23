6effa452e505e5540135fdc4599c5fba28a49756
AkifPinarci <mehmetakifpinarci@gmail.com>
2022-10-22 17:02:22 -0700

    sc and flags added

--- bin/ps/ProcessList.cpp
+++ bin/ps/ProcessList.cpp
@@ -26,13 +26,14 @@ ProcessList::ProcessList(int argc, char **argv)
     : POSIXApplication(argc, argv)
 {
     parser().setDescription("Output system process list");
+    parser().registerFlag('l', "list", "Shows process priority");
 }
 
 ProcessList::Result ProcessList::exec()
 {
     const ProcessClient process;
     String out;
-
+    if (arguments().get("list")){
     // Print header
     out << "ID  PARENT  USER GROUP STATUS     CMD                              PRIORITY\r\n";
 
@@ -55,7 +56,30 @@ ProcessList::Result ProcessList::exec()
             out << line;
         }
     }
+    }
+    else {
+       out << "ID  PARENT  USER GROUP STATUS     CMD\r\n";
 
+    // Loop processes
+    for (ProcessID pid = 0; pid < ProcessClient::MaximumProcesses; pid++)
+    {
+        ProcessClient::Info info;
+
+        const ProcessClient::Result result = process.processInfo(pid, info);
+        if (result == ProcessClient::Success)
+        {
+            DEBUG("PID " << pid << " state = " << *info.textState);
+
+            // Output a line
+            char line[128];
+            snprintf(line, sizeof(line),
+                    "%3d %7d %4d %5d %10s %32s\r\n",
+                     pid, info.kernelState.parent,
+                     0, 0, *info.textState, *info.command);
+            out << line;
+        }
+    } 
+    }
     // Output the table
     write(1, *out, out.length());
     return Success;
--- bin/renice/Renice.cpp
+++ bin/renice/Renice.cpp
@@ -28,8 +28,9 @@ Renice::Renice(int argc, char **argv)
     : POSIXApplication(argc, argv)
 {
     parser().setDescription("Change the priority of a process");
-    parser().registerPositional("PID", "The ID of the process to change the priority of");
+    parser().registerFlag('n', "new", "Changes process priority");
     parser().registerPositional("PRIO", "The new priority for the process");
+    parser().registerPositional("PID", "The ID of the process to change the priority of");
 }
 
 Renice::~Renice()
@@ -39,30 +40,33 @@ Renice::~Renice()
 Renice::Result Renice::exec()
 {
     // Get input as integer
-    int pid = atoi(arguments().get("PID"));
-    int prio = atoi(arguments().get("PRIO"));
+    if (arguments().get("new")){
 
-    const ProcessClient process;
-    ProcessClient::Info info;
+        int prio = atoi(arguments().get("PRIO"));
+        int pid = atoi(arguments().get("PID"));
 
-    // Make sure the process is valid
-    const ProcessClient::Result result = process.processInfo(pid, info);
-    if (result == ProcessClient::Success)
-    {
-        // Valid process
-        if(renice(pid, prio)) 
+        const ProcessClient process;
+        ProcessClient::Info info;
+
+        // Make sure the process is valid
+        const ProcessClient::Result result = process.processInfo(pid, info);
+        if (result == ProcessClient::Success)
         {
-            ERROR("failed to change priority: " << strerror(errno));
+            // Valid process
+            if(renice(pid, prio)) 
+            {
+                ERROR("failed to change priority: " << strerror(errno));
+                return IOError;
+            }
+        }
+
+        else
+        {
+            // Invalid process - return an error
+            ERROR("invalid process id: " << arguments().get("PID") << "'");
             return IOError;
         }
-    }
 
-    else
-    {
-        // Invalid process - return an error
-        ERROR("invalid process id: " << arguments().get("PID") << "'");
-        return IOError;
     }
-
     return Success;
 }
